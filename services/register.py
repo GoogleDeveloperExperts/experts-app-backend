3318da82ca86c72e2025b188cb41c823b36a7824
Patrick Martinent <patrick.martinent@gmail.com>
2016-08-24 22:11:03 +0530

    registering the services

--- services/register.py
+++ services/register.py
@@ -1,14 +1,14 @@
 """ Endpoint api server registration."""
 
 import endpoints
-from .web_endpoints import ActivityPostService
-from .web_endpoints import ActivityRecordService
+from .web_endpoints import ActivityDetailService
+from .web_endpoints import ActivityMasterService
 from .web_endpoints import AccountService
 from .web_endpoints import ProductGroupService
 from .web_endpoints import ActivityTypeService
 from .web_endpoints import ActivityGroupService
 
-application = endpoints.api_server([ActivityRecordService, ActivityPostService,
+application = endpoints.api_server([ActivityMasterService, ActivityDetailService,
                                     AccountService, ProductGroupService,
                                     ActivityTypeService, ActivityGroupService],
                                    restricted=False)
